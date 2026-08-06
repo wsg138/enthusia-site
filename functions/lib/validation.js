7ace85fdc999fe6188eb777ec9525623c520f47e
P2wn <racecarboy77@gmail.com>
2026-08-06 10:50:26 -0400

    Accept canonical Floodgate UUIDs

--- functions/lib/validation.js
+++ functions/lib/validation.js
@@ -1,4 +1,4 @@
-const CANONICAL_UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const CANONICAL_UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export function isCanonicalUuid(value) {
   return typeof value === "string" && CANONICAL_UUID.test(value);
