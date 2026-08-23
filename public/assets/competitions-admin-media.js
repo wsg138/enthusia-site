20df2e5e20325aaafcc0ea778868017c3348f86d
P2wn <racecarboy77@gmail.com>
2026-08-22 22:53:24 -0400

    Load admin deep-link and readiness bootstrap

--- public/assets/competitions-admin-media.js
+++ public/assets/competitions-admin-media.js
@@ -1,5 +1,6 @@
 import "./competitions-admin-workspace.js";
 import "./competitions-admin-tools.js";
+import "./competitions-admin-bootstrap.js";
 
 const workspaceStyles = document.createElement("link");
 workspaceStyles.rel = "stylesheet";
