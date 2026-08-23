a352202514a154b2b3ba9024f469052ba975de18
P2wn <racecarboy77@gmail.com>
2026-08-22 22:55:31 -0400

    Remove superseded notification channel migration

--- migrations/0018_competition_notification_channels.sql
+++
@@ -1,6 +0,0 @@
-PRAGMA foreign_keys = ON;
-
--- Track delivery per channel so retrying one failed notification channel never
--- duplicates a notification that already reached the other channel.
-ALTER TABLE competition_notification_outbox ADD COLUMN bridge_delivered_at TEXT;
-ALTER TABLE competition_notification_outbox ADD COLUMN discord_delivered_at TEXT;
